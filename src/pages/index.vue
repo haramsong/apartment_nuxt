0072b9e2fafb95aececab93790ea4f6a2f2683d2
haramsong <alakazam1324@gmail.com>
2023-10-10 17:56:44 +0900

    index 페이지 작업

---
+++ src/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page-container>
+    <q-page>
+        
+    </q-page>
+  </q-page-container>
+</template>
+
+<script setup>
+import { useQuasar } from 'quasar';
+const $q = useQuasar();
+
+</script>
