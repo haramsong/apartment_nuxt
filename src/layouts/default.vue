f585578b7234be78278aafd54fed9bc54a64a840
haramsong <alakazam1324@gmail.com>
2023-10-10 17:58:30 +0900

    기본 레이아웃 추가

---
+++ src/layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+    <q-layout view="hHh lpR fFf" class="bg-grey-1">
+        <LayoutsHeader />
+        <LayoutsAsideNav />
+        <q-page-container>
+            <router-view />
+        </q-page-container>
+    </q-layout>
+</template>
