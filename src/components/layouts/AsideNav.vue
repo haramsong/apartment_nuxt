44c9fe090f5e9dde6994b8ba39814e5b9ba6c4ec
haramsong <alakazam1324@gmail.com>
2023-10-11 17:57:16 +0900

    사이드바 반응형

--- src/components/layouts/AsideNav.vue
+++ src/components/layouts/AsideNav.vue
@@ -1,7 +1,7 @@
 <template>
-    <q-drawer v-model="drawer" :breakpoint="200" :mini="miniState || leftDrawerOpen" show-if-above bordered class="YL__drawer" :width="260">
-            <q-list padding>
-                <q-btn flat dense round @click="store.toggleLeftDrawer()" aria-label="Menu" icon="menu" />
+    <q-drawer v-if="!miniState" v-model="drawer" :breakpoint="500" :mini="leftDrawerOpen" show-if-above bordered class="YL__drawer" :width="260">
+        <q-btn z-max flat dense round @click="store.toggleLeftDrawer()" aria-label="Menu" icon="menu" />
+        <q-list padding>
                 <q-item v-for="link in links1" :key="link.text" class="YL__drawer-item"  v-ripple clickable>
                     <q-item-section avatar>
                         <q-icon color="white" :name="link.icon" />
@@ -12,6 +12,20 @@
                 </q-item>
             </q-list>
     </q-drawer>
+    <q-drawer v-else v-model="drawer" :mini="!leftDrawerOpen" :breakpoint="0" show-if-above bordered class="YL__drawer" :width="160">
+                            <q-btn flat dense round @click="store.toggleLeftDrawer()" aria-label="Menu" icon="menu" />
+    
+            <q-list padding>
+                    <q-item v-for="link in links1" :key="link.text" class="YL__drawer-item"  v-ripple clickable>
+                        <q-item-section avatar>
+                            <q-icon color="white" :name="link.icon" />
+                        </q-item-section>
+                        <q-item-section>
+                            <q-item-label>{{ link.text }}</q-item-label>
+                        </q-item-section>
+                    </q-item>
+                </q-list>
+        </q-drawer>
 </template>
 
 <script setup lang="ts">
@@ -23,31 +37,28 @@ const $q = useQuasar();
 
 const store = useDrawerStore();
 
+
 const drawer = ref<boolean>(true);
 
 const { leftDrawerOpen } = storeToRefs(store);
 const miniState = computed(() => store.miniState);
 
-// name: 'MyLayout',
-
-// setup() {
-// const leftDrawerOpen = ref<boolean>(false)
-const search = ref('')
-
-// function toggleLeftDrawer() {
-//     leftDrawerOpen.value = !leftDrawerOpen.value
-// }
+// watch(
+//     miniState, () => {
+//         if (miniState.value === true) {
+//             leftDrawerOpen.value = true;
+//         }
+//         else {
+//             leftDrawerOpen.value = false;
+//         }
+//     }
+// )
 
 const links1 = [
     { icon: 'home', text: 'Home' },
     { icon: 'whatshot', text: 'Trending' },
     { icon: 'subscriptions', text: 'Subscriptions' }
 ];
-
-// const miniState = computed(() => {
-//     return $q.screen.width < 500
-// })
-
 </script>
 
 <style lang="sass">
