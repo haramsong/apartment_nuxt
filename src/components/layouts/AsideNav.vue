0deb2e40d604fe02fa3b2ddcc1103abe3aba9bbb
haramsong <alakazam1324@gmail.com>
2023-10-12 10:32:34 +0900

    사이드바 반응형 완료 & 사이드바 버튼 스타일 적용

--- src/components/layouts/AsideNav.vue
+++ src/components/layouts/AsideNav.vue
@@ -1,7 +1,7 @@
 <template>
-    <q-drawer v-if="!miniState" v-model="drawer" :breakpoint="500" :mini="leftDrawerOpen" show-if-above bordered class="YL__drawer" :width="260">
-        <q-btn z-max flat dense round @click="store.toggleLeftDrawer()" aria-label="Menu" icon="menu" />
+    <q-drawer v-if="!miniState" v-model="drawer" :breakpoint="-100" :mini="leftDrawerOpen" show-if-above bordered class="YL__drawer" :width="260">
         <q-list padding>
+            <q-btn z-max flat dense round @click="store.toggleLeftDrawer()" aria-label="Menu" class="YL__drawer-button" icon="menu" />
                 <q-item v-for="link in links1" :key="link.text" class="YL__drawer-item"  v-ripple clickable>
                     <q-item-section avatar>
                         <q-icon color="white" :name="link.icon" />
@@ -12,19 +12,18 @@
                 </q-item>
             </q-list>
     </q-drawer>
-    <q-drawer v-else v-model="drawer" :mini="!leftDrawerOpen" :breakpoint="0" show-if-above bordered class="YL__drawer" :width="160">
-                            <q-btn flat dense round @click="store.toggleLeftDrawer()" aria-label="Menu" icon="menu" />
-    
+    <q-drawer v-else v-model="smallDrawer" :mini="!leftDrawerOpen" :breakpoint="-100" show-if-above bordered class="YL__drawer" :width="160">
             <q-list padding>
-                    <q-item v-for="link in links1" :key="link.text" class="YL__drawer-item"  v-ripple clickable>
-                        <q-item-section avatar>
-                            <q-icon color="white" :name="link.icon" />
-                        </q-item-section>
-                        <q-item-section>
-                            <q-item-label>{{ link.text }}</q-item-label>
-                        </q-item-section>
-                    </q-item>
-                </q-list>
+                <q-btn flat dense round @click="store.toggleLeftDrawer()" aria-label="Menu" class="YL__drawer-button" icon="menu" />
+                <q-item v-for="link in links1" :key="link.text" class="YL__drawer-item"  v-ripple clickable>
+                    <q-item-section avatar>
+                        <q-icon color="white" :name="link.icon" />
+                    </q-item-section>
+                    <q-item-section>
+                        <q-item-label>{{ link.text }}</q-item-label>
+                    </q-item-section>
+                </q-item>
+            </q-list>
         </q-drawer>
 </template>
 
@@ -37,8 +36,8 @@ const $q = useQuasar();
 
 const store = useDrawerStore();
 
-
-const drawer = ref<boolean>(true);
+const drawer = computed(() => $q.screen.gt.xs);
+const smallDrawer = computed(() => $q.screen.width < 300);
 
 const { leftDrawerOpen } = storeToRefs(store);
 const miniState = computed(() => store.miniState);
@@ -72,6 +71,11 @@ const links1 = [
     font-size: 1.5rem
     margin: 1.5rem 0
     padding: 1rem
+    color: white
+  
+  &__drawer-button
+    font-size: 1.5rem
+    top: 0
     color: white
 
   &__drawer-footer-link
