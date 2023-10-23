081a33014e89ae7a9ecaea36d05c5a9bb6210f1d
haramsong <alakazam1324@naver.com>
2023-10-23 15:37:53 +0900

    홈 화면 예시 완성

--- src/components/layouts/Card.vue
+++ src/components/layouts/Card.vue
@@ -1,13 +1,44 @@
 <template>
-        <q-card class="my-card q-ma-lg">
+        <q-card class="my-card q-ma-xl">
             <q-card-section>
                 <span class="text-h6 text-bold">주민게시판</span>
-                <span class="text-subtitle2  float-right">전체보기</span>
+                <span class="text-grey-7 text-subtitle2 float-right">전체보기</span>
               </q-card-section>
               <q-card-section class="q-pt-none">
-                <div class="text-subtitle2 text-bold">안녕하세요. 103동 102호 입니다.</div>
-                <div class="text-subtitle2 text-bold">이거 어떻게 쓰나요?</div>
-                <div class="text-subtitle2 text-bold">부탁드립니다.제발 쓰레기좀 여기에....</div>
+                <div class="text-subtitle2">안녕하세요. 103동 102호 입니다.</div>
+                <div class="text-subtitle2">이거 어떻게 쓰나요?</div>
+                <div class="text-subtitle2">부탁드립니다.제발 쓰레기좀 여기에....</div>
               </q-card-section>
         </q-card>
+        <q-card class="my-card q-ma-xl">
+                <q-card-section>
+                    <span class="text-h6 text-bold">나눔장터</span>
+                    <span class="text-grey-7 text-subtitle2 float-right">전체보기</span>
+                  </q-card-section>
+                  <q-card-section class="q-pt-none">
+                    <div class="text-subtitle2">소파 가져가실분 가져가세요.</div>
+                    <div class="text-subtitle2">책상이랑 의자 교환하실분?</div>
+                    <div class="text-subtitle2">화분 2개 처분합니다.</div>
+                  </q-card-section>
+            </q-card>
+            <q-card class="my-card q-ma-xl">
+                <q-card-section>
+                    <span class="text-h6 text-bold">건의사항</span>
+                    <span class="text-grey-7 text-subtitle2 float-right">전체보기</span>
+                  </q-card-section>
+                  <q-card-section class="q-pt-none">
+                    <div class="text-subtitle2">
+                        <span>베란다 열고 담배좀 못피게 해주세요.</span>
+                        <span class="text-bold text-red-6 float-right">접수 전</span>
+                    </div>
+                    <div class="text-subtitle2">
+                        <span>주차장에 페인트가 벗겨졌네요</span>
+                        <span class="text-bold text-blue-6 float-right">처리 중</span>
+                    </div>
+                    <div class="text-subtitle2">
+                        <span>무개념 주차차량에 대한 규제좀 마련해주세요.</span>
+                        <span class="text-bold text-green-6 float-right">처리 완료</span>
+                    </div>
+                  </q-card-section>
+            </q-card>
 </template>
