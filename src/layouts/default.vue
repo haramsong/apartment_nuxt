3eccbdf81e2785cee4077ca7bb2da1149e7b7ce3
haramsong <alakazam1324@naver.com>
2023-10-25 17:56:21 +0900

    소메뉴 페이지 헤더 추가

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -4,9 +4,26 @@
             <LayoutsHeader />
             <LayoutsAsideNav />
             <q-page-container>
+                <main class="container">
+                    <template v-if="$route.path !== '/'">
+                        <section class="page-header">
+                            <div class="page-header__title">
+                            <h2 class="sub_title1">
+                                주민게시판
+                            </h2>
+                            </div>
+                        </section>
+                    </template>
+                </main>
                 <slot />
             </q-page-container>
         </div>
     </q-layout>
 </template>
+
+<script lang="ts" setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+</script>
 
