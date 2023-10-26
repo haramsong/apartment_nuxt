77d42ab3ca37a642247a1ed31c859d75c16c6bf2
key9559 <key9559@mygarage.kr>
2023-10-26 18:19:25 +0900

    헤더 변경중

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -1,7 +1,22 @@
 <template>
-    <q-header elevated class="bg-white text-grey-8 q-py-xs" height-hint="58">
+    <header class="sc-header">
+        <div class="flex-btw-wrap">
+            <div class="left-contents">
+                <button type="button" class="m-gnb-toggle-btn" @click="store.toggleLeftDrawer()" v-if="!$q.screen.gt.xs">햄버거버튼</button>
+            </div>
+            <div class="center-contents">
+                <h1 class="sc-logo">
+                    <a href="">
+                        <img src="/img/logo.svg" />
+                    </a>
+                </h1>
+            </div>
+            <div class="right-contents"></div>
+        </div>
+    </header>
+    <!-- <q-header elevated class="bg-white text-grey-8 q-py-xs" height-hint="58">
         <q-toolbar>
-            <q-btn z-max flat dense round @click="store.toggleLeftDrawer()" v-if="!$q.screen.gt.xs" aria-label="Menu" class="YL__drawer-button" icon="menu" />
+            <q-btn z-max flat dense round aria-label="Menu" class="YL__drawer-button" icon="menu" />
             <q-btn size="sm" flat no-caps no-wrap class="YL__logo" v-if="$q.screen.gt.xs">
                 <q-icon name="img:/img/logo.svg" class="YL__logo" />
             </q-btn>
@@ -30,7 +45,7 @@
                 </q-btn>
             </div>
         </q-toolbar>
-    </q-header>
+    </q-header> -->
 </template>
 
 <script setup lang="ts">
