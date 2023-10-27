d0240c293e04ea3f912e57e447bf921969fdee5d
haramsong <alakazam1324@naver.com>
2023-10-27 17:47:47 +0900

    모달 container 공통화

---
+++ src/components/modal/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <aside class="modal">
+    <div class="modal-bg"></div>
+    <div class="modal-container">
+      <div v-if="props.type === 'alert'">
+        <ModalAlert />
+      </div>
+      <div v-if="props.type === 'confirm'">
+        <ModalConfirm />
+      </div>
+    </div>
+  </aside>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  type: {
+    type: String,
+    required: true,
+  },
+})
+</script>
