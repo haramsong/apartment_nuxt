50b136d32dfab553239d0b91d3945bd3986b9390
송하람 <alakazam1324@naver.com>
2024-03-15 17:05:14 +0900

    간단한 회원가입 폼

---
+++ src/pages/auth/sign_in.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>회원가입 폼</section>
+  <form action="">
+    <input type="text">
+    <input type="text">
+    <input type="text">
+    <input type="text">
+    <button type="submit">가입</button>
+    <button type="button" @click="cancelSignIn()">취소</button>
+  </form>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const cancelSignIn = () => {
+  router.push('/auth/login')
+}
+</script>
